4651693fe7dc97acff1302fe535cc3921c44edea
Davi Ferreira <daviferreira1155@gmail.com>
2019-11-13 21:00:14 -0200

    Tela Usuario

--- ProjetoCRUD/src/univs/edu/usuario/UsuarioDAO.java
+++ ProjetoCRUD/src/univs/edu/usuario/UsuarioDAO.java
@@ -1,8 +1,10 @@
 
 package univs.edu.usuario;
 
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 import univs.edu.util.HibernateUtil;
 
 
@@ -19,6 +21,36 @@ public class UsuarioDAO {
         sessao.close();
     }
 
+    public void excluir(Usuario usuario){
+       sessao = HibernateUtil.getSessionFactory().openSession();
+        
+       transacao = sessao.beginTransaction(); 
+       sessao.delete(usuario);
+       transacao.commit();
+       sessao.close();
+    
+    }
+    
+    public Usuario pesquisar(int id){
+         sessao = HibernateUtil.getSessionFactory().openSession(); 
+       transacao = sessao.beginTransaction();
+        Usuario usuario = (Usuario) sessao.
+               createCriteria(Usuario.class).add( 
+               Restrictions.eq("idUsuario", id))
+                .uniqueResult();
+        return usuario;
+                 
+    }
+    
+        public List<Usuario> pesquisar(){
+         sessao = HibernateUtil.getSessionFactory().openSession(); 
+       transacao = sessao.beginTransaction();
+        List<Usuario> usuarios = sessao.
+               createCriteria(Usuario.class).list(); 
+        return usuarios;
+                
+    }
+    
     
     }
 
